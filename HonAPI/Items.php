804f18f38a2c4a554e2e2b523197f05967dd1316
David Normington <david.normo@gmail.com>
2014-05-27 09:17:07 +0100

    Finished items

--- HonAPI/Items.php
+++ HonAPI/Items.php
@@ -10,4 +10,37 @@ class Items extends Request
 		$this->setController('items');
 		$this->setMethod('all');
 	}
+
+	/**
+	 * Restrict request to within a certain shop
+	 * 
+	 * @param String $name
+	 */
+	public function setShopName($name)
+	{
+		$this->setMethod('shop');
+		$this->setProperty('name', $name);	
+		return $this;
+	}
+
+	/**
+	 * Restrict by shop slot number
+	 *
+	 * @param Int|String $slotNum
+	 */
+	public function setShopSlot($slotNum)
+	{
+		$this->setMethod('shop');
+		$this->setProperty('slot', $slotNum);
+		return $this;
+	}
+
+	/**
+	 * Get all items categorised by shop
+	 */
+	public function setShopAll()
+	{
+		$this->setMethod('shop/all');
+		return $this;
+	}
 }
